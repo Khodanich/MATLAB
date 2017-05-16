c7b64e1cc1f02c9b45618cf5c3a94f797e3fe0df
AHodanich <AHodanich@yandex.ru>
2017-05-16 15:59:57 +0300

    M

--- ph/Test_BD.m
+++ ph/Test_BD.m
@@ -20,7 +20,7 @@ s1(1:lengthCorr/2) = s;
 s1((lengthCorr/2 + 1):lengthCorr) = s;
 
 NoiseDb = 25;
-AmpSignal = 10;%%амплитуда сигнала
+AmpSignal = 5;%%амплитуда сигнала
 code1xTemp1 = s1;
 code1xTemp = s1;
 
@@ -49,64 +49,104 @@ for i = 1:lengthCorr        %%генерация помех
   SignalNoise(i) = Asin*sin(2*pi*Fs/Fd*i); 
   SignalNoise1(i) = Asin1*sin(2*pi*Fs1/Fd1*i);
 end
-
-for i = 1:lengthCorr
-    code1xTemp(i) = SignalNoise(i) +  SignalNoise1(i) + code1xTemp(i) ;
-end
-
-corrr = Correlation(code1xTemp,s);
-corrr = abs(corrr);
-plot(corrr);
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-phref = 180;
-phSignal1 = 30;
-phSignal2 = 60;
-phSin = 30;
-phSin1 = 80;
+phref = 0;
 
-s1_reference = s1 + wgn(1,lengthCorr,NoiseDb,'complex');                    %%опорный сигнал
+
+phSignal1 = 50;
+phSignal2 = 60;
+
+phSin = 120;
+phSin1 = 90;
+
+s1_reference = s1*exp(1i*phref/57.3) + wgn(1,lengthCorr,NoiseDb,'complex');                    %%опорный сигнал
 s1n_noise =  s1*exp(1i*phSignal1/57.3) + wgn(1,lengthCorr,NoiseDb,'complex');%% сигнал1 с шумом сдвинут на 30 градусов
-s2n_noise =  s1*exp(1i*phSignal2/57.3) + wgn(1,lengthCorr,NoiseDb,'complex');%%сигнал 2 с шумом сдвинут на 60 градусов
+%s2n_noise =  s1*exp(1i*phSignal2/57.3) + wgn(1,lengthCorr,NoiseDb,'complex');%%сигнал 2 с шумом сдвинут на 60 градусов
+
+
 
 sin_noise_1 =  SignalNoise*exp(1i*phSin/57.3) + wgn(1,lengthCorr,NoiseDb,'complex');%% помеха1
-sin_noise_2 =  SignalNoise1*exp(1i*phSin1/57.3) + wgn(1,lengthCorr,NoiseDb,'complex');%% помеха2
+%sin_noise_2 =  SignalNoise1*exp(1i*phSin1/57.3);% + wgn(1,lengthCorr,NoiseDb,'complex');%% помеха2
 
-s1n_mix = s1n_noise + sin_noise_1 + sin_noise_2;    %%сумма сигнала1 и помех
-s2n_mix = s2n_noise + sin_noise_1 + sin_noise_2;    %%сумма сигнала2 и помех
-s_reference_interf = s1_reference + sin_noise_1 + sin_noise_2;%% сумма опорного сигнала и помех
+%%% только белый шум
+s1_WhiteNoise = s1*exp(1i*phSignal1/57.3) + wgn(1,lengthCorr,NoiseDb,'complex'); 
+%%одна помеха
+s_one_interference = s1*exp(1i*phSignal1/57.3) + SignalNoise*exp(1i*phSin/57.3) + wgn(1,lengthCorr,NoiseDb,'complex'); 
+s1_reference_one_interf = s1*exp(1i*phref/57.3) +  SignalNoise + wgn(1,lengthCorr,NoiseDb,'complex');
+
+%%две помехи
+
+s1n_mix = s1*exp(1i*phSignal1/57.3) + SignalNoise*exp(1i*phSin/57.3)+ SignalNoise1*exp(1i*phSin1/57.3)+ wgn(1,lengthCorr,NoiseDb,'complex');     %%сумма сигнала1 и помех
+s2n_mix = s1*exp(1i*phSignal2/57.3) + SignalNoise*exp(1i*phSin/57.3)+ SignalNoise1*exp(1i*phSin1/57.3)+ wgn(1,lengthCorr,NoiseDb,'complex');   %%сумма сигнала2 и помех
+s_referense_interf = s1*exp(1i*phref/57.3) + SignalNoise + SignalNoise1 + wgn(1,lengthCorr,NoiseDb,'complex');%% сумма опорного сигнала и помех
+
 
 mass_temp = 0:359;
+%%воздействеи только белого шума
+% for j1_ctr = 1:1:360     
+%     s2n_noise_interf_WhiteNoise = s1_reference*exp(1i*((deg2rad(j1_ctr - 1))));
+%     %Signal_sdvig_phase2 = s2n_noise*exp(1i*((deg2rad(j1_ctr - 1))));
+%     s2n_noise_interf_sum = s2n_noise_interf_WhiteNoise + s1_WhiteNoise; 
+%     %Signal_sdvig_phase_Sum = Signal_sdvig_phase2 + sin_noise_1 + sin_noise_2;
+%     
+%     Signal_sdvig_phase_22_sin = abs(Correlation(s2n_noise_interf_sum,s)); 
+%   
+%     %Energy1(j1_ctr) = sum(abs(s2n_noise_interf).^2);
+%     q_phase_Quality_sin(j1_ctr) = Quality(Signal_sdvig_phase_22_sin,lengthCorr);   
+%     %max_phase_max(j1_ctr) =  max(Signal_sdvig_phase_22_sin);
+% end
+ 
+%%воздействие одной помехи
+% for j1_ctr = 1:1:360     
+%     s2n_noise_interf = s1_reference_one_interf*exp(1i*((deg2rad(j1_ctr - 1))));
+%     %Signal_sdvig_phase2 = s2n_noise*exp(1i*((deg2rad(j1_ctr - 1))));
+%     s2n_noise_interf_sum = s2n_noise_interf + s_one_interference; 
+%     %Signal_sdvig_phase_Sum = Signal_sdvig_phase2 + sin_noise_1 + sin_noise_2;
+%     
+%     Signal_sdvig_phase_22_sin = abs(Correlation(s2n_noise_interf_sum,s)); 
+%   
+%     %Energy1(j1_ctr) = sum(abs(s2n_noise_interf).^2);
+%     q_phase_Quality_sin(j1_ctr) = Quality(Signal_sdvig_phase_22_sin,lengthCorr);   
+%     %max_phase_max(j1_ctr) =  max(Signal_sdvig_phase_22_sin);
+% end
+% 
+% plot(mass_temp,q_phase_Quality_sin);
 
-for j1_ctr = 1:1:360     
-    s2n_noise_interf = s2n_mix*exp(1i*((deg2rad(j1_ctr - 1))));
-    %Signal_sdvig_phase2 = s2n_noise*exp(1i*((deg2rad(j1_ctr - 1))));
-    s2n_noise_interf = s2n_noise_interf + s_reference_interf; 
-    %Signal_sdvig_phase_Sum = Signal_sdvig_phase2 + sin_noise_1 + sin_noise_2;
-    
-    %Signal_sdvig_phase_22_sin = abs(Correlation(s2n_noise_interf,s)); 
-    Energy1(j1_ctr) = sum(abs(s2n_noise_interf).^2);
-    %q_phase_Quality_sin(j1_ctr) = Quality(Signal_sdvig_phase_21_sin,lengthCorr);   
+%%воздействие двух помех
+for i = 1:1:360
+    s1n_noise_interf = s1n_mix*exp(1i*((deg2rad(j - 1))));    
+    for j = 1:1:360
+        s2n_noise_interf = s2n_mix*exp(1i*((deg2rad(i - 1))));
+        %Signal_sdvig_phase2 = s2n_noise*exp(1i*((deg2rad(j1_ctr - 1))));
+        s2n_noise_interf = s1n_noise_interf + s2n_noise_interf + s_referense_interf; 
+        %Signal_sdvig_phase_Sum = Signal_sdvig_phase2 + sin_noise_1 + sin_noise_2;
+
+        %Signal_sdvig_phase_22_sin = abs(Correlation(s2n_noise_interf,s)); 
+        Energy1(i) = sum(abs(s2n_noise_interf).^2);
+        %q_phase_Quality_sin(j1_ctr) = Quality(Signal_sdvig_phase_21_sin,lengthCorr);    
+    end  
+    MaxEnergy(j) = max(Energy1);
 end
+[X,Y]=meshgrid(mass_temp,mass_temp);
+plot3(X,Y,MaxEnergy);
+% 
+% plot(mass_temp,Energy1);
+% figure;
+% 
+% for j1_ctr = 1:1:360 
+%     
+%     s1n_noise_interf = s1n_mix*exp(1i*((deg2rad(j1_ctr - 1))));
+%     %Signal_sdvig_phase2 = s2n_noise*exp(1i*((deg2rad(j1_ctr - 1))));
+%     %s1n_noise_interf = s1n_noise_interf + s_reference_interf; 
+%     Signal_sdvig_phase_Sum = s1n_noise_interf + s_reference_interf;
+%     
+%     Signal_sdvig_phase_21_sin = abs(Correlation(Signal_sdvig_phase_Sum,s)); 
+%     
+%     %Energy(j1_ctr) = sum(abs(s1n_noise_interf).^2);
+%     q_phase_Quality_sin(j1_ctr) = Quality(Signal_sdvig_phase_21_sin,lengthCorr);
+%    
+% end
 
 
-plot(mass_temp,Energy1);
-figure;
-
-for j1_ctr = 1:1:360 
-    
-    s1n_noise_interf = s1n_mix*exp(1i*((deg2rad(j1_ctr - 1))));
-    %Signal_sdvig_phase2 = s2n_noise*exp(1i*((deg2rad(j1_ctr - 1))));
-    %s1n_noise_interf = s1n_noise_interf + s_reference_interf; 
-    Signal_sdvig_phase_Sum = s1n_noise_interf + s_reference_interf;
-    
-    Signal_sdvig_phase_21_sin = abs(Correlation(Signal_sdvig_phase_Sum,s)); 
-    
-    %Energy(j1_ctr) = sum(abs(s1n_noise_interf).^2);
-    q_phase_Quality_sin(j1_ctr) = Quality(Signal_sdvig_phase_21_sin,lengthCorr);
-   
-end
-
-
-plot(mass_temp,q_phase_Quality_sin);
+% plot(mass_temp,q_phase_Quality_sin);
 
